74ed4e43613e5ec04e73e871c9d064cd1bfbf90d
sara-tustra <77685910+sara-tustra@users.noreply.github.com>
2023-02-07 00:11:23 -0300

    Some urls, it has problems

--- server/api/urls.py
+++ server/api/urls.py
@@ -5,6 +5,14 @@ from .views import (
     CustomerDetail,
     CustomerUpdate,
     CustomerDelete,
+    TournamentCreate,
+    TournamentList,
+    PlayerCreate,
+    PlayerList,
+    RoundCreate,
+    RoundList,
+    MatchCreate,
+    MatchList,
 )
 
 
@@ -14,4 +22,12 @@ urlpatterns = [
     path("<int:pk>/", CustomerDetail.as_view(), name="retrieve-customer"),
     path("update/<int:pk>/", CustomerUpdate.as_view(), name="update-customer"),
     path("delete/<int:pk>/", CustomerDelete.as_view(), name="delete-customer"),
+    path("tournament/create/", TournamentCreate.as_view(), name="create-tournament"),
+    path("tournament", TournamentList.as_view()),
+    path("player/create/", PlayerCreate.as_view(), name="create-player"),
+    path("player", PlayerList.as_view()),
+    path("round/create/", RoundCreate.as_view(), name="create-round"),
+    path("round", RoundList.as_view()),
+    path("match/create/", MatchCreate.as_view(), name="create-match"),
+    path("match", MatchList.as_view()),
 ]
